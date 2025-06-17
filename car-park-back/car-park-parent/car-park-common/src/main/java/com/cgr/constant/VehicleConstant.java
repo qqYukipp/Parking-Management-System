2b3062977b3858f4f307ea81e2c0994cd85b39f3
WDG411 <re.life411@gmail.com>
2025-06-17 14:32:38 +0800

    .

---
+++ car-park-back/car-park-parent/car-park-common/src/main/java/com/cgr/constant/VehicleConstant.java
@@ -0,0 +1,12 @@
+package com.cgr.constant;
+
+
+// 车辆类型常量
+public class VehicleConstant {
+
+    public static final int TYPE_INTERNAL = 1;   // 内部车
+    public static final int TYPE_MONTHLY = 2;    // 月租车
+    public static final int TYPE_TEMPORARY = 3;  // 临时车
+    public static final int TYPE_BLACKLIST = 4;  // 黑名单车辆
+
+}
